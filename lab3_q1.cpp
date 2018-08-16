cd66ace41b6b01ece6dc73e0dbd4a140b6ecb9fc
vivekdwived <42137907+vivekdwived@users.noreply.github.com>
2018-08-16 20:29:23 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,27 @@
+//first include the library
+#include <iostream>
+using namespace std;;
+// now use main function 
+int main()
+//compilation starts after this 
+{ cout<<"hello" <<endl;
+
+int a,b;//declaring the variables together
+
+ cout<<"size of integer is "<<sizeof(a)<<endl;
+
+float c=1.2 ;
+
+cout<<"float=1.2"<<endl;;
+
+cout<<"the size of float "<<sizeof(c)<<endl;
+
+cout<<"size of char is "<<sizeof(char)<<endl;
+
+bool boolvar=true;
+
+cout<< "size of bool "<<sizeof(bool)<<endl;
+double d;
+cout<<"size of double is "<<sizeof(d);
+
+return 0;}
---
+++ lab3_q2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>//library
+
+using namespace std;
+//then main function
+
+int main ()
+ { 
+int a,b,c,d,sum,product,divide,modulus;
+
+
+cout<<"enter 1st no."<<endl;
+cin>>a;
+
+cout<<"enter 2nd no."<<endl;
+cin>>b;
+cout<< "3rd no."<<endl;
+cin>>c;
+
+cout<<"4th no."<<endl;
+cin>>d;
+
+sum=a+b+c+d;
+cout<<"sum all="<<sum<<endl;
+
+product=a*b*c*d;
+cout<<"product="<<product<<endl;
+
+divide =c/d;
+cout<<"divide 3rd and 4th  then ="<<divide<<endl;
+ 
+modulus=a%b;
+cout<<"modulus of 1st & 2nd is "<<modulus<<endl;
+return 0;
+}
+
+
+
+
